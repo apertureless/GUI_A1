11a221c4921e6ce2c59a838888b5716aa0e24df5
netghost03 <netghost03@gmail.com>
2014-10-16 10:39:44 +0200

    First commit

---
+++ Wein Lager/src/wein/lager/WeinLager.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
+ */
+package wein.lager;
+
+/**
+ *
+ * @author NetGhost03
+ */
+public class WeinLager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
