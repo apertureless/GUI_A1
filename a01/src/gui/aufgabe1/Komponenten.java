377812ae37cf6f92b3fa12fda049fdf8aa0d79f1
netghost03 <netghost03@gmail.com>
2014-10-16 14:30:47 +0200

    paintComponent overwritten

--- a01/src/gui/aufgabe1/Komponenten.java
+++ a01/src/gui/aufgabe1/Komponenten.java
@@ -4,6 +4,7 @@
  */
 package gui.aufgabe1;
 
+import java.awt.*;
 import javax.swing.JPanel;
 
 /**
@@ -15,10 +16,50 @@ public class Komponenten extends JPanel {
     final Diagramm diagramm;
     final Legende legende;
     
+    private static final Color FARBE_HINTERGRUND = Color.white;
+    
+    /**
+     * Erzeugt die Komponenten des Fensters
+     */
     public Komponenten() {
         
+        // Das Diagramm wird erzeugt.
         this.diagramm = new Diagramm();
+        
+        // Die Legende wird erzeugt.
         this.legende = new Legende();
+    }
+    
+    /**
+     * Methode legt die Schriftart sowie die Schriftgröße fest.
+     * @param g 
+     */
+    public void setTextStil(Graphics2D g) {
+        // Festlegen von Schriftart und Größe
+        int fontSize = (int)Math.round(1.5 * this.getWidth() / 72.0);
+        Font font = new Font("Arial", Font.PLAIN, fontSize);
+        g.setFont(font);
+    }
+    
+    @Override
+    public void paintComponent(Graphics g) {
+        
+        Graphics2D g2 = (Graphics2D) g;
+        
+        // Aktiviere Antialiasing (Schriftenglaettung)
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+        RenderingHints.VALUE_ANTIALIAS_ON);
+        // Setze weisse Hintergrundfarbe
+        
+        this.setBackground(FARBE_HINTERGRUND);
+        
+        this.setTextStil(g2);
+        
+        this.diagramm.setWerte(this.getSize(), g2);
+        
+        super.paintComponent(g2);
+        
+        this.diagramm.erzeugeDiagramm();
     }
     
 }
