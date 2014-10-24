7c38408ba3a6b0f05caff87e2a5b0d06b86223f0
netghost03 <netghost03@gmail.com>
2014-10-24 16:39:09 +0200

    Change window title

--- a01/src/gui/aufgabe1/Aufgabe1.java
+++ a01/src/gui/aufgabe1/Aufgabe1.java
@@ -15,7 +15,7 @@ public class Aufgabe1 {
     
     public static void main(String[] args) throws IOException {
         
-        Fenster f = new Fenster("GUI Aufgabe" , 20, 20, 500, 300);
+        Fenster f = new Fenster("GUI Aufgabe 1" , 20, 20, 500, 300);
         f.setVisible(true);   
         f.zeichneKomponenten(new Diagramm());
     }
