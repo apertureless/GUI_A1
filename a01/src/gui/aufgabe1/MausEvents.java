6e880a4ed23c4fde47dc649bb489d7f0d2ebf69e
netghost03 <netghost03@gmail.com>
2014-10-24 15:07:02 +0200

    Add abstract methods

--- a01/src/gui/aufgabe1/MausEvents.java
+++ a01/src/gui/aufgabe1/MausEvents.java
@@ -4,10 +4,49 @@
  */
 package gui.aufgabe1;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 /**
  *
  * @author Jakub Juszczak <juszczak.j@googlemail.com>
  */
-public class MausEvents {
+public class MausEvents implements MouseListener, MouseMotionListener {
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
