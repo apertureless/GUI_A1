1f916708ff897e4444d2c9c822f2146d1ab4096f
netghost03 <netghost03@gmail.com>
2014-10-25 12:55:28 +0200

    Add class for stadium

---
+++ a01/src/gui/aufgabe1/Stadium.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
+ */
+package gui.aufgabe1;
+
+/**
+ *
+ * @author Jakub Juszczak <juszczak.j@googlemail.com>
+ */
+public class Stadium extends Diagramm {
+    
+    public String name;
+    public int beginn;
+    public double dauer;
+    public int position;
+    public Object farbe;
+    public double xPos, yPos, breite, hoehe;
+    
+      public Stadium(String name, Double dauer, Object farbe, 
+                        double breite, double hoehe, double yPos, 
+                        int position) {
+          
+        this.name = name;
+        this.dauer = dauer;
+        this.farbe = farbe;
+        this.breite = breite;
+        this.hoehe = hoehe;
+        this.yPos = yPos;
+        this.position = position;
+    }
+    
+    public void setBeginn(int beginn) {
+        this.beginn = beginn;
+    }
+    
+    public void setXPos(double x) {
+        this.xPos = x;
+    }
+    
+    public double getXPos() {
+        return this.xPos;
+    }
+    
+    public double getYPos() {
+        return this.yPos;
+    }
+    
+    public boolean istImBreich(int x, int y) {
+        boolean istInStadiumX = (x >= this.xPos && (x <= (this.xPos + this.breite)));
+        boolean istInStadiumY = (y >= this.yPos && (y <= (this.yPos + this.hoehe)));
+        
+        return (istInStadiumY && istInStadiumY);
+    }
+    
+}
