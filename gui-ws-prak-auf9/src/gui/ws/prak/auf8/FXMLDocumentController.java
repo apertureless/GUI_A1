dfa62b94e94f7bdd345456ed3a5a83680d014738
netghost03 <netghost03@gmail.com>
2015-01-16 00:24:12 +0100

    Fix project name

--- gui-ws-prak-auf9/src/gui/ws/prak/auf8/FXMLDocumentController.java
+++ gui-ws-prak-auf9/src/gui/ws/prak/auf8/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gui.ws.prak.auf9;
+package gui.ws.prak.auf8;
 
 import java.net.URL;
 import java.util.ResourceBundle;
